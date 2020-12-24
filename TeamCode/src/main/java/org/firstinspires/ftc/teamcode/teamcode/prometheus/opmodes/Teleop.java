8da5a29982bcd40eab69525512d90a6361adf5fd
Lakshmi Avanthsa <lavanthsa@gmail.com>
2020-12-24 15:45:45 -0500

    lakshmi v1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/Teleop.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.teamcode.prometheus.opmodes;
+
+public class Teleop extends LinearOpMode {
+        // Declare drive motors
+        private DcMotor frontLeft;
+        private DcMotor frontRight;
+        private DcMotor backLeft;
+        private DcMotor backRight;
+}

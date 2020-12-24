2bdbcef6be8cc314502d4664ef5179315f189e12
Lakshmi Avanthsa <lavanthsa@gmail.com>
2020-12-24 17:43:17 -0500

    lakshmi v2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/Teleop.java
@@ -1,9 +1,44 @@
 package org.firstinspires.ftc.teamcode.teamcode.prometheus.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "lakshmiTeleop")
 public class Teleop extends LinearOpMode {
         // Declare drive motors
         private DcMotor frontLeft;
         private DcMotor frontRight;
         private DcMotor backLeft;
         private DcMotor backRight;
+
+
+        @Override
+        public void runOpMode() throws InterruptedException {
+                // Initialize drive motors
+                frontLeft = hardwareMap.dcMotor.get("frontLeft");
+                frontRight = hardwareMap.dcMotor.get("frontRight");
+                backLeft = hardwareMap.dcMotor.get("backLeft");
+                backRight = hardwareMap.dcMotor.get("backRight");
+
+                // If drive motors are given full power, robot would spin because of the motors being in
+                // opposite directions. So reverse one
+                backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+                // Wait until start button is pressed
+                waitForStart();
+
+                // Repeatedly run code in here until stop button is pressed
+                while(opModeIsActive()) {
+                        // Tank drive
+                        frontLeft.setPower(-gamepad1.left_stick_y);
+                        frontRight.setPower(-gamepad1.right_stick_y);
+                        backLeft.setPower(-gamepad1.left_stick_y);
+                        backRight.setPower(-gamepad1.right_stick_y);
+
+                        // Give hardware a chance to catch up
+                        idle();
+                }
+        }
 }

85b3d2dd7a3f1d7099726e38af7a41a7eaab2b52
Prattyush Giriraj <prattyushgiri@gmail.com>
2021-01-01 20:02:36 -0500

    Hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/EncoderPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/EncoderPosition.java
@@ -16,9 +16,6 @@ public class EncoderPosition extends LinearOpMode {
     DriveTrain dt;
     TrackerWheels tw;
 
-
-
-
     @Override
     public void runOpMode() throws InterruptedException {
         dt = new DriveTrain(this, true);
@@ -34,38 +31,13 @@ public class EncoderPosition extends LinearOpMode {
 
             dt.setFromAxis(gamepad1.right_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
 
-
             telemetry.addData("BL",dt.backLeft.getCurrentPosition() );
             telemetry.addData("BR",dt.backRight.getCurrentPosition() );
             telemetry.addData("FR",dt.frontRight.getCurrentPosition() );
             telemetry.addData("FL",dt.frontLeft.getCurrentPosition() );
 
-
-
             telemetry.update();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         }
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/robot/TrackerWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/robot/TrackerWheels.java
@@ -8,13 +8,12 @@ import org.firstinspires.ftc.teamcode.teamcode.prometheus.lib.Pos;
 import org.firstinspires.ftc.teamcode.teamcode.prometheus.robot.Encoder;
 
 public class TrackerWheels {
-
     private OpMode opMode;
 //
 //    private Encoder x1;
 //    private Encoder x2;
 //    private Encoder y1;
-double oldX1;
+    double oldX1;
     double oldX2;
     double oldY;
 
@@ -24,7 +23,6 @@ double oldX1;
         this.opMode = opMode;
 
         pos = new Pos();
-
 
 //        //TODO fix wheel radius
 //        x1 = new Encoder();
@@ -54,66 +52,35 @@ double oldX1;
         double distanceX;
         double distanceY;
         double turnDistance;
-
         double totalCir = 9*Math.PI;
         Angle turn;
 
+        x1 = (double) (encX1-oldX1)/8192 * 2.28346 * Math.PI;
+        x2 = (double) (encX2-oldX2)/8192 * 2.28346 * Math.PI;
+        y = (double) (encY-oldY)/8192 * 2.28346 * Math.PI;
 
+        distanceX = ((x1+x2)/2);
+        distanceY = y;
+        turn = new Angle().setDegrees((((x1-x2)/totalCir)*360)/2);
+        Angle finalAngle = new Angle().setDegrees(pos.angle.getDegrees()+turn.getDegrees());
+        Angle change = new Angle().setDegrees((pos.angle.getDegrees()+finalAngle.getDegrees())/2);
 
-                x1 = (double) (encX1-oldX1)/8192 * 2.28346 * Math.PI;
-                x2 = (double) (encX2-oldX2)/8192 * 2.28346 * Math.PI;
-                y = (double) (encY-oldY)/8192 * 2.28346 * Math.PI;
+        pos.x += (distanceX * Math.cos(change.getRadians())) -  Math.sin(change.getRadians())*distanceY;
+        pos.y += (distanceY * Math.cos(change.getRadians())) +  Math.sin(change.getRadians())*distanceX;
+        pos.angle = finalAngle;
 
+        opMode.telemetry.addData("distanceX", distanceX);
+        opMode.telemetry.addData("distanceY", distanceY);
+        opMode.telemetry.addData("angle", turn.deg());
+        opMode.telemetry.addData("positionX",pos.x);
+        opMode.telemetry.addData("positionY",pos.y);
+        opMode.telemetry.addData("positionAngle",pos.angle.getDegrees());
 
+        oldX1 = encX1;
+        oldX2 = encX2;
+        oldY = encY;
 
-
-                distanceX = ((x1+x2)/2);
-                distanceY = y;
-                turn = new Angle().setDegrees((((x1-x2)/totalCir)*360)/2);
-                Angle finalAngle = new Angle().setDegrees(pos.angle.getDegrees()+turn.getDegrees());
-                Angle change = new Angle().setDegrees((pos.angle.getDegrees()+finalAngle.getDegrees())/2);
-
-                pos.x += (distanceX * Math.cos(change.getRadians())) -  Math.sin(change.getRadians())*distanceY;
-                pos.y += (distanceY * Math.cos(change.getRadians())) +  Math.sin(change.getRadians())*distanceX;
-                pos.angle = finalAngle;
-
-                opMode.telemetry.addData("distanceX", distanceX);
-                opMode.telemetry.addData("distanceY", distanceY);
-                opMode.telemetry.addData("angle", turn.deg());
-                opMode.telemetry.addData("positionX",pos.x);
-                opMode.telemetry.addData("positionY",pos.y);
-                opMode.telemetry.addData("positionAngle",pos.angle.getDegrees());
-
-
-
-
-                oldX1 = encX1;
-                oldX2 = encX2;
-                oldY = encY;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            }
-        }
+    }
+}
 
 

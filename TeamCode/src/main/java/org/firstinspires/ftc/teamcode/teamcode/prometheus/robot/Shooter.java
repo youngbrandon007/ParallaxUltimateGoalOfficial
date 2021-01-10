cbac64767a0b6e9e142c28906e75bf3fca073145
DESKTOP-VDU8Q3E\iyeng <iyengar.prathik@gmail.com>
2021-01-10 15:40:41 -0500

    Prathik

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/robot/Shooter.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Shooter {
-
+    //Prathik wrote this 
    public DcMotor shooter;
 
     private OpMode opMode;

8437abe700001e551c63ce1ca3d3a537575a3d0a
Lakshmi Avanthsa <lavanthsa@gmail.com>
2021-02-14 14:33:04 -0500

    lakshmi v28

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/auto/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/prometheus/opmodes/auto/Autonomous.java
@@ -56,6 +56,8 @@ public class Autonomous extends LinearOpMode {
     camera.startTensorFlow();
     camera.servoDown();
 
+    loopTime.reset();
+    dt.resetTrackerWheels();
 
     while (opModeIsActive()) {
         if (loopTime.milliseconds() > 50) {
